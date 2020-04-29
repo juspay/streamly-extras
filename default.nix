442c2db11869938dd0a86f9d2afa3890966b8433
Paul Victor Raj <paulvictor@gmail.com>
2020-04-29 15:48:38 +0530

    Updating streamly version

--- default.nix
+++ default.nix
@@ -6,8 +6,8 @@ let
   nixpkgs = fetchFromGitHub {
     owner = "NixOS";
     repo = "nixpkgs";
-    rev = "e8a36cdf57193e56514aa6eeff936159372f0ace";
-    sha256 = "1jxdqphacpzkvwpkw67w1222jnmyplzall4n9sdwznyipxz6bqsv";
+    rev = "20.03";
+    sha256 = "0182ys095dfx02vl2a20j1hz92dx3mfgz2a6fhn31bqlp1wa8hlq";
   };
   config = {
     packageOverrides = pkgs: rec {
@@ -19,12 +19,20 @@ let
             self.ghc.withPackages;
           streamly-extras =
             self.callPackage ./streamly-extras.nix { };
+          fusion-plugin-types =
+            pkgs.haskell.lib.dontCheck
+              (self.callCabal2nix "fusion-plugin-types" (pkgs.fetchgit {
+                url = "https://github.com/composewell/fusion-plugin-types.git";
+                rev = "1a7e1c39b4496543b2dc95d59aafbf44041554f1";
+                sha256 = "1mmph6gawi4cbsqmswawi1c951f6pq41qfqjbvnygm36d2qfv64i";
+                fetchSubmodules = true;
+              }) { });
           streamly =
             pkgs.haskell.lib.dontCheck
               (self.callCabal2nix "streamly" (pkgs.fetchgit {
                 url = "https://github.com/composewell/streamly.git";
-                rev = "83cbe5d14cc8c383aa7f2975743742a0bc16e35e";
-                sha256 = "1apil2qz5a0bc6y82azb80njg0wp44hj3d8cxq5m51y8l66pvqva";
+                rev = "500d187b8fb5b6b1bc424725dd179650fb41c49c";
+                sha256 = "175cqn0sxg8r32f3dd0alkivkvjrwhmwm7xkmf4ki2j68smmjc3y";
                 fetchSubmodules = true;
               }) { });
 
@@ -46,7 +54,6 @@ in
             ghcid
             hindent
             hlint
-            stylish-haskell
           ] ++ [ zlib ] ++ attrs.buildInputs;
         })
         else drv
